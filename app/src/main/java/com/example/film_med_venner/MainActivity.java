e305186fe7132527afea6bf8327c509fd1356717
SilasRindorf <s195474@student.dtu.dk>
2020-09-28 12:30:54 +0200

    initial commit

---
+++ app/src/main/java/com/example/film_med_venner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.film_med_venner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
