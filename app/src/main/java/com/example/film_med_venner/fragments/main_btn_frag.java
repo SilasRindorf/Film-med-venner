a4f040f76358972fe5878138fbc78f0062bad3ad
SilasRindorf <s195474@student.dtu.dk>
2020-09-28 15:32:31 +0200

    made main frag

---
+++ app/src/main/java/com/example/film_med_venner/fragments/main_btn_frag.java
@@ -0,0 +1,16 @@
+package com.example.film_med_venner.fragments;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.film_med_venner.R;
+
+public class main_btn_frag extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle SavedInstanceState){
+        return inflater.inflate(R.layout.main_btn_frag, container, false);
+    }
+}
