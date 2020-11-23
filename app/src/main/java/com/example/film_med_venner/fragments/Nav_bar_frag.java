8f7aac0aa74439e61d048fbe0c6f02f2e3f75ec1
SilasRindorf <s195474@student.dtu.dk>
2020-11-23 14:40:08 +0100

    nav frag updated

--- app/src/main/java/com/example/film_med_venner/fragments/Nav_bar_frag.java
+++ app/src/main/java/com/example/film_med_venner/fragments/Nav_bar_frag.java
@@ -1,5 +1,4 @@
 package com.example.film_med_venner.fragments;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,21 +9,27 @@ import android.widget.ImageButton;
 import androidx.fragment.app.Fragment;
 
 import com.example.film_med_venner.R;
+import com.example.film_med_venner.activities.ChatActivity;
 import com.example.film_med_venner.activities.MainActivity;
 
 public class Nav_bar_frag extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.frag_nav_bar, container, false);
-        ImageButton btn = (ImageButton) view.findViewById(R.id.home_btn);
+        int[] btnIDs = {R.id.home_btn};
+        ImageButton btn;
+        for (int btnID : btnIDs) {
+            btn = (ImageButton) view.findViewById(btnID);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    intent.setClass(getActivity(), MainActivity.class);
+                }
+            });
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(getActivity(),MainActivity.class);
-            }
-        });
+        }
+
 
         return view;
     }
