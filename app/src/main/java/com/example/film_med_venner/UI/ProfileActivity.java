c6614474fd386825782ae3c5ba8e12d1d07e9e4c
Sejr <sejrlba@gmail.com>
2020-11-25 14:18:43 +0100

    Added ReviewAdapter to be used in ProfileActivity. For now it is just sample data, but it's something!

--- app/src/main/java/com/example/film_med_venner/UI/ProfileActivity.java
+++ app/src/main/java/com/example/film_med_venner/UI/ProfileActivity.java
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.film_med_venner.R;
 import com.example.film_med_venner.UI.fragments.Nav_bar_frag;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    LinearLayout l_layout_review;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_main);
         Fragment frag = new Nav_bar_frag();
         addFrag(R.id.nav_bar_container,frag);
+
+        l_layout_review = findViewById(R.id.linearLayout_review);
+        l_layout_review.setOnClickListener(this);
     }
 
     private void addFrag(int id, Fragment fragment) {
@@ -25,5 +31,14 @@ public class ProfileActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(id, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == l_layout_review){
+            setContentView(R.layout.activity_review);
+            Intent intent = new Intent(this, ReviewActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/film_med_venner/UI/ReviewActivity.java
@@ -0,0 +1,123 @@
+package com.example.film_med_venner.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+
+import com.example.film_med_venner.DAO.Movie;
+import com.example.film_med_venner.DAO.Rating;
+import com.example.film_med_venner.DAO.Review;
+import com.example.film_med_venner.DAO.WatchlistItem;
+import com.example.film_med_venner.R;
+import com.example.film_med_venner.UI.Adapters.ReviewAdapter;
+import com.example.film_med_venner.UI.fragments.Nav_bar_frag;
+import com.example.film_med_venner.interfaces.IReview;
+import com.example.film_med_venner.interfaces.IMovie;
+import com.example.film_med_venner.interfaces.IRating;
+import com.example.film_med_venner.interfaces.IReview;
+import com.example.film_med_venner.interfaces.IWatchlistItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewActivity extends AppCompatActivity {
+    GridView gridView;
+    private ReviewAdapter ReviewAdapter;
+    private Context ctx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+        ctx = this;
+
+        Fragment frag = new Nav_bar_frag();
+        addFrag(R.id.nav_bar_container,frag);
+
+        gridView = findViewById(R.id.gridView);
+
+    }
+
+    private void addFrag(int id, Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(id, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setupHomeFeed(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setupHomeFeed(false);
+    }
+
+    void setupHomeFeed(boolean run) {
+        AsyncTask asyncTask = new AsyncTask() {
+            List<IReview> items = new ArrayList<>();
+            String errorMsg = null;
+
+            @Override
+            protected void onPreExecute() {
+            }
+
+            @Override
+            protected Object doInBackground(Object... arg0) {
+                try {
+
+                    // Dummy data
+                    IMovie movie = new Movie("Bee Movie", "info", new ArrayList<String>(), new String[3], "poster");
+                    IReview review = new Review("Very bee, much buzz", "Kurger Bing", movie);
+
+                    ArrayList<IReview> feedList = new ArrayList<IReview>();
+                    feedList.add(review);
+                    feedList.add(review);
+                    feedList.add(review);
+                    feedList.add(review);
+
+
+
+
+                    items =  feedList;
+                    return null;
+                } catch (Exception e) {
+                    //    errorMsg = e.getMessage();
+                    e.printStackTrace();
+                    return e;
+                }
+            }
+
+            @Override
+            protected void onCancelled() {
+                super.onCancelled();
+            }
+
+            @Override
+            protected void onPostExecute(Object titler) {
+                ReviewAdapter = new ReviewAdapter(ctx, items);
+                gridView.setAdapter(ReviewAdapter);
+                gridView.setVisibility(View.VISIBLE);
+            }
+
+        };
+
+        if (run) {
+            asyncTask.execute();
+        } else {
+            asyncTask.cancel(true);
+        }
+    }
+}
