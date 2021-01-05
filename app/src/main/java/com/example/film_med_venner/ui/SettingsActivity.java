57b604d6a08d8f941bf9b8bdb949fca04b148fe0
Sejr <sejrlba@gmail.com>
2021-01-05 14:26:39 +0100

    Edited the settings page in regards to the .xml file

--- app/src/main/java/com/example/film_med_venner/ui/SettingsActivity.java
+++ app/src/main/java/com/example/film_med_venner/ui/SettingsActivity.java
@@ -12,12 +12,10 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.film_med_venner.R;
 import com.example.film_med_venner.ui.fragments.Nav_bar_frag;
-import com.example.film_med_venner.ui.settingActivities.HomefeedSettingActivity;
-import com.example.film_med_venner.ui.settingActivities.ProfileSettingActivity;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView profSet, homeSet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +24,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         Fragment frag = new Nav_bar_frag();
         addFrag(R.id.nav_bar_container,frag);
 
-
-        profSet = findViewById(R.id.text_profile_settings);
-        profSet.setOnClickListener(this);
-        homeSet = findViewById(R.id.text_homefeed_settings);
-        homeSet.setOnClickListener(this);
     }
 
     private void addFrag(int id, Fragment fragment) {
@@ -42,14 +35,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View view) {
-        if (view == profSet) {
-            Intent intent = new Intent(this, ProfileSettingActivity.class);
-            startActivity(intent);
-        } else if (view == homeSet) {
-            Intent intent = new Intent(this, HomefeedSettingActivity.class);
-            startActivity(intent);
-        }
-
 
     }
 }
