2e3b71eebcfd12be91b53cddcd0d39f9d6f0dad2
SilasRindorf <s195474@student.dtu.dk>
2021-01-06 02:27:47 +0100

    did database stuff more good

--- app/src/main/java/com/example/film_med_venner/databases/Database.java
+++ app/src/main/java/com/example/film_med_venner/databases/Database.java
@@ -1,17 +1,29 @@
 package com.example.film_med_venner.databases;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
 import com.example.film_med_venner.interfaces.IDatabase;
 import com.example.film_med_venner.interfaces.IHomeFeedItems;
 import com.example.film_med_venner.interfaces.IMovie;
 import com.example.film_med_venner.interfaces.IProfile;
 import com.example.film_med_venner.interfaces.IRating;
 import com.example.film_med_venner.interfaces.IReview;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static android.content.ContentValues.TAG;
 
 
 //TODO should be handled in thread
@@ -26,19 +38,38 @@ public class Database implements IDatabase {
         return instance;
     }
     private Database(){
-        //FirebaseApp.initializeApp()
         db =  FirebaseFirestore.getInstance();
-        //addUser("Bob Mclaren");
-        System.out.println("Users: " + db.collection("users").get().toString());
+        addUser("Bob Mclaren");
     }
     public boolean  addUser(String name){
         HashMap<String, Object> user = new HashMap();
         user.put("name",name);
-        db.collection("users").add(user);
+        db.collection("users").add(user)
+        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "User added with ID: " + documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding user", e);
+            }
+        });
         return true;
     }
     @Override
     public IProfile getProfile(int id) {
+        System.out.println("Users: " + db.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    for (QueryDocumentSnapshot doc : task.getResult()){
+                        System.out.println("User: " + doc.getData());
+                    }
+                }
+            }
+        }));
         return null;
     }
 
