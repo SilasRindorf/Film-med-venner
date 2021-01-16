2f3b800c9f4952791f17aca1bc6c13b04293f7f5
Sejr <sejrlba@gmail.com>
2021-01-16 17:52:39 +0100

    The foundation for getting information from a Facebook user has been laid. Now it just needs to be created and added to the database

--- app/src/main/java/com/example/film_med_venner/ui/MainActivity.java
+++ app/src/main/java/com/example/film_med_venner/ui/MainActivity.java
@@ -16,6 +16,7 @@ import com.example.film_med_venner.controllers.Controller_User;
 import com.example.film_med_venner.databases.Database;
 import com.example.film_med_venner.interfaces.IDatabase;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -26,6 +27,8 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONObject;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,18 +42,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login_main);
 
 
-
         auth = Controller_User.getInstance();
 
 
-
-
         //Comment out to not skip log in screen
-       if (Controller_User.getInstance().isLoggedIn() || Database.getInstance().isFacebookUserLoginValid()) {
+        if (Controller_User.getInstance().isLoggedIn() || Database.getInstance().isFacebookUserLoginValid()) {
             Intent intent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(intent);
         }
-
 
 
         EditText username_input_editText = findViewById(R.id.input_username);
@@ -84,68 +83,68 @@ public class MainActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
         LoginButton continue_using_fb_btn = findViewById(R.id.btn_signup_using_facebook);
-        continue_using_fb_btn.setPermissions("public_profile");
+        continue_using_fb_btn.setPermissions(Arrays.asList("public_profile", "email"));
         continue_using_fb_btn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-        @Override
+            @Override
             public void onSuccess(LoginResult loginResult) {
-            try {
-                Database.getInstance().loginWithFacebookUser(loginResult.getAccessToken(), () -> {
-                    GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                        @Override
-                        public void onCompleted(JSONObject object, GraphResponse response) {
-                            try {
-                                String id = object.getString("id");
-                                String name = object.getString("name");
-                                String email = object.getString("email");
-                                String image_url = "http://graph.facebook.com/" + id + "/picture?type=large";
-                                Log.e("TAG",id);
-                                Log.e("TAG",name);
-                                Log.e("TAG",email);
-                                Log.e("TAG",image_url);
-                            } catch (Exception e){
-                                Log.e("TAG", e.toString());
-                            }
-                        }
-                    });
-                    Bundle parameters = new Bundle();
-                    parameters.putString("fields", "id,name,email,image_url");
-                    request.setParameters(parameters);
-                    request.executeAsync();
+                Bundle parameters = new Bundle();
+                try {
+                    Database.getInstance().loginWithFacebookUser(loginResult.getAccessToken(), () -> {
+                        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    String id = object.getString("id");
+                                    String name = object.getString("name");
+                                    String email = object.getString("email");
+                                    String image_url = "http://graph.facebook.com/" + id + "/picture?type=large&access_token=" + loginResult.getAccessToken().getToken();
+                                    Log.e("TAG", id);
+                                    Log.e("TAG", name);
+                                    Log.e("TAG", email);
+                                    Log.e("TAG", image_url);
+                                    //TODO Tilføj fb bruger i db måske vha. param bundle?
 
-                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    /*ProfileDTO fbProfile = new ProfileDTO(
-                            request.getParameters().getString("id"),
-                            request.getParameters().getString("name"),
-                            0, 0, 0, 0, 0, 0
-                            //request.getParameters().getString("email"),
-                            //request.getParameters().getString("image_url")
-                    );
-                    Database.getInstance().addFacebookUser(fbProfile, new RunnableErrorUI() {
-                        @Override
-                        public void run() {
-                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                            startActivity(intent);
-                        }
-                        @Override
-                        public void handleError(IDatabase.DatabaseException e) {
-                            Toast.makeText(MainActivity.this, "Invalid Facebook Profile!", Toast.LENGTH_LONG).show();
-                        }
-                    });*/
-                });
-            } catch (IDatabase.DatabaseException e) {
-                Toast.makeText(MainActivity.this,"Failed to log into Facebook",Toast.LENGTH_LONG).show();
+                                    /*ProfileDTO fbProfile = new ProfileDTO(id, name, email, image_url)
+                                    Database.getInstance().addFacebookUser(fbProfile, new RunnableErrorUI() {
+                                        @Override
+                                        public void run() {
+                                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                            startActivity(intent);
+                                        }
+
+                                        @Override
+                                        public void handleError(IDatabase.DatabaseException e) {
+                                            Toast.makeText(MainActivity.this, "Invalid Facebook Profile!", Toast.LENGTH_LONG).show();
+                                        }
+                                    });*/
+                                } catch (Exception e) {
+                                    Log.e("TAG", e.toString());
+                                }
+                            }
+                        });
+                        /*parameters.putString("id",id);
+                        parameters.putString("name",name);
+                        parameters.putString("email",email);
+                        parameters.putString("image_url",image_url);*/
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                        Toast.makeText(MainActivity.this, "Succesfully logged in with your Facebook account", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    });
+                } catch (IDatabase.DatabaseException e) {
+                    Toast.makeText(MainActivity.this, "Failed to log into Facebook", Toast.LENGTH_LONG).show();
+                }
             }
-        }
 
             @Override
             public void onCancel() {
-                Log.e("MainActFacebook","Facebook log in request canceled");
+                Log.e("MainActFacebook", "Facebook log in request canceled");
             }
 
             @Override
             public void onError(FacebookException error) {
-                Log.e("MainActFacebook","Error on Facebook login",error);
+                Log.e("MainActFacebook", "Error on Facebook login", error);
             }
         });
 
@@ -156,6 +155,7 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
     public void onStart() {
         super.onStart();
     }
