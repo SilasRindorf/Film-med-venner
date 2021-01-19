f6464b83909c9febe8263c277d0ee9a6df33c027
SilasRindorf <s195474@student.dtu.dk>
2021-01-19 23:19:08 +0100

    Added map to home act

--- app/src/main/java/com/example/film_med_venner/interfaces/IReview.java
+++ app/src/main/java/com/example/film_med_venner/interfaces/IReview.java
@@ -1,7 +1,12 @@
 package com.example.film_med_venner.interfaces;
+
+import java.util.Date;
+
 public interface IReview extends IHomeFeedItems {
     //fx 0-10 stars
     int getRating();
     void setReviewID(String id);
     String getReview();
+    Date getCreationDate();
+    void setCreationDate(Date creationDate);
 }
