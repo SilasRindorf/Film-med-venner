b78713e92b3725addd9de3997e23475cff6033a8
SilasRindorf <s195474@student.dtu.dk>
2021-01-19 14:55:51 +0100

    Added email

--- app/src/main/java/com/example/film_med_venner/DTO/ProfileDTO.java
+++ app/src/main/java/com/example/film_med_venner/DTO/ProfileDTO.java
@@ -7,7 +7,7 @@ public class ProfileDTO {
     private String name;
     private String pictureURL;
     private String mvGPrefs;
-
+    private String email;
 
     public ProfileDTO() {
 
@@ -18,6 +18,23 @@ public class ProfileDTO {
         this.ID = profile.getID();
         this.name = profile.getName();
         this.mvGPrefs = profile.getmvGPrefs();
+        this.email = profile.getEmail();
+    }
+
+    public String getMvGPrefs() {
+        return mvGPrefs;
+    }
+
+    public void setMvGPrefs(String mvGPrefs) {
+        this.mvGPrefs = mvGPrefs;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPictureURL() {
